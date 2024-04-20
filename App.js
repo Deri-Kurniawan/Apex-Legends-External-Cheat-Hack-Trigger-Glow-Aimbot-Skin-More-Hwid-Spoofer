9a5001744772624d69fa2c4ff3d3b42cf1442b2b
Deri-Kurniawan <deri.netuchi@gmail.com>
2024-04-21 03:59:58 +0700

    reinitialize project with expo@50.0.14

--- App.js
+++ App.js
@@ -1,82 +1,20 @@
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { TailwindProvider } from "tailwindcss-react-native";
-import TimelineScreen from "./screens/TimelineScreen";
-import { useFonts } from "expo-font";
-import ProfileScreen from "./screens/ProfileScreen";
-import ExploreScreen from "./screens/ExploreScreen";
-import ReelsScreen from "./screens/ReelsScreen";
-import MarketPlaceScreen from "./screens/MarketPlaceScreen";
-import HomeFeedLikerScreen from "./screens/HomeFeedLikerScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-
-const Stack = createNativeStackNavigator();
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "transparent",
-  },
-};
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [loaded] = useFonts({
-    Billabong: require("./assets/fonts/FontsFree-Net-Billabong.ttf"),
-  });
-
-  if (!loaded) return null;
-
   return (
-    <TailwindProvider>
-      <NavigationContainer theme={theme}>
-        <Stack.Navigator
-          screenOptions={{ headerShown: false }}
-          initialRouteName="Login"
-        >
-          <Stack.Screen
-            options={{ animation: "none" }}
-            name="Timeline"
-            component={TimelineScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "none" }}
-            name="Explore"
-            component={ExploreScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "none" }}
-            name="Reels"
-            component={ReelsScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "none" }}
-            name="MarketPlace"
-            component={MarketPlaceScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "none" }}
-            name="Profile"
-            component={ProfileScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "slide_from_right" }}
-            name="HomeFeedLiker"
-            component={HomeFeedLikerScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "fade_from_bottom" }}
-            name="Login"
-            component={LoginScreen}
-          />
-          <Stack.Screen
-            options={{ animation: "fade_from_bottom" }}
-            name="Register"
-            component={RegisterScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </TailwindProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
